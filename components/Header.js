18b2f9ab39061d58dec176ce88cea0fecccf7bea
Farrukh Zeb <FarrukhXeb@gmail.com>
2020-12-22 18:22:59 +0500

    Fixed the Header Links by using passHref

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import { Navbar, Nav } from 'react-bootstrap';
+import Link from 'next/link';
 function Header() {
     return (
         <Navbar bg='light' expand='lg'>
@@ -6,12 +7,20 @@ function Header() {
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
                 <Nav className='mr-auto'>
-                    <Nav.Link href='/'>Home</Nav.Link>
-                    <Nav.Link href='/about'>About</Nav.Link>
+                    <Link href='/' passHref>
+                        <Nav.Link>Home</Nav.Link>
+                    </Link>
+                    <Link href='/about' passHref>
+                        <Nav.Link>About</Nav.Link>
+                    </Link>
                 </Nav>
                 <Nav>
-                    <Nav.Link href='/login'>Login</Nav.Link>
-                    <Nav.Link href='/signup'>Sign Up</Nav.Link>
+                    <Link href='/login' passHref>
+                        <Nav.Link>Login</Nav.Link>
+                    </Link>
+                    <Link href='/signup' passHref>
+                        <Nav.Link>Sign Up</Nav.Link>
+                    </Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
